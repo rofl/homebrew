4bf58b45241e52c32d5b326967d7a07f69fc586d
Robert Flick <rofl@imove.org>
2010-09-06 20:39:08 +0200

    Updated Formula to 1.9

--- Library/Formula/dcled.rb
+++ Library/Formula/dcled.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Dcled <Formula
-  url 'http://www.jeffrika.com/~malakai/dcled/dcled-1.8.tgz'
+  url 'http://www.jeffrika.com/~malakai/dcled/dcled-1.9.tgz'
   homepage 'http://www.jeffrika.com/~malakai/dcled/index.html'
-  md5 '1f0c32964ff521c2f39156cca5ef7a29'
+  md5 '7cffd6585a5d355626d60e2127599dd0'
 
   depends_on 'libhid'
 
@@ -12,7 +12,9 @@ class Dcled <Formula
       s.change_make_var! 'INSTALLDIR', prefix + 'bin'
       s.change_make_var! 'CC', ENV['CC']
       s.change_make_var! 'CFLAGS', ENV['CFLAGS'] + " -I#{HOMEBREW_PREFIX}/include"
-      s.change_make_var! 'LDFLAGS', ENV['LDFLAGS'] + ' -lm -lhid'
+      if ENV['LDFLAGS'] != nil
+         s.change_make_var! 'LDFLAGS', ENV['LDFLAGS'] + ' -lm -lhid'
+      end
     end
 
     system "make && make install"
